c54b539c94f335584247a016bb88eb38ced47b94
Reyhan Jovie <achimonchi.reyhan@gmail.com>
2020-04-03 14:53:38 +0700

    add user

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const
     mongoose = require('mongoose'),
     bodyParser = require('body-parser');
 
+const User = require('./User');
 
 const app = express();
 
@@ -34,9 +35,32 @@ app.get("/:id", (req,res,next)=>{
 })
 // add user
 app.post("/", (req,res,next)=>{
-    res.status(200).json({
-        message : "Add new user"
+    const body = req.body;
+    
+    const user = new User({
+        _id : mongoose.Types.ObjectId(),
+        u_name : body.u_name,
+        u_umur : body.u_umur,
+        u_email : body.u_email,
+        u_password : body.u_password,
     })
+
+    user.save()
+        .then(data =>{
+            res.status(200).json({
+                message : "Add new user",
+                data
+            })        
+        })
+        .catch(err=>{
+            res.status(500).json({
+                message : "Something gonna wrong!",
+                error : err
+            })
+        })
+
+
+    
 })
 // update user
 app.put("/:id", (req,res,next)=>{
